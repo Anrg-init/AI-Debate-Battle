aec5da5786e843747a1c7e26f148e84410a79607
anrg-init <electronicdm565@gmail.com>
2026-09-18 19:05:30 +0530

    fix/setting_prod

--- Debate_Project/settings.py
+++ Debate_Project/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres.pwtmxzeanmddfftddzhi'),
         'PASSWORD': os.getenv('SUPABASE_DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'db.pwtmxzeanmddfftddzhi.supabase.co'),
+        'HOST': os.getenv('DB_HOST', 'aws-0-ap-northeast-2.pooler.supabase.com'),
         'PORT': os.getenv('DB_PORT', '5432'),
         'CONN_MAX_AGE': int(os.getenv('DB_CONN_MAX_AGE', '60')),
     }
